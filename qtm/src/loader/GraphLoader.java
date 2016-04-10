6dfaa5f55ff0215107f276ed3e1c7d1af5dbae22
zach <zacharias.holland@gmail.com>
2016-04-10 16:21:50 -0700

    Added documentation to the GraphLoader class.
    Also refactored the edges only loader to be a bit more efficient.

--- qtm/src/loader/GraphLoader.java
+++ qtm/src/loader/GraphLoader.java
@@ -12,31 +12,40 @@ import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Random;
 import java.util.StringTokenizer;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+/**
+ * Utility class that provides methods to load a graph from a file into a JUNG graph.
+ */
 public class GraphLoader {
 
+    /**
+     * Creates a JUNG graph from a graph in a file in the .paj format.
+     *
+     * @param fileLocation The location of the graph file.
+     * @return A graph object that represents the graph specified in the file.
+     * @throws IOException
+     */
     public static Graph<Vertex<Integer>, Edge<String>> createGraphFromFile(String fileLocation) throws IOException {
         Graph<Vertex<Integer>, Edge<String>> graph = new SparseGraph<>();
         Path path = Paths.get(fileLocation);
 
         Stream<String> lines = Files.lines(path);
 
-        List<String> vertexStringLines = lines
-                                                 .filter((line) -> {
-                                                     Pattern p = Pattern.compile("\\d+ \"\\d+\"");
-                                                     Matcher m = p.matcher(line);
-                                                     return m.matches();
-                                                 })
-                                                 .collect(Collectors.toList());
+        // Get all the vertices of the graph.
+        List<String> vertexStringLines = lines.filter((line) -> {
+            Pattern p = Pattern.compile("\\d+ \"\\d+\"");
+            Matcher m = p.matcher(line);
+            return m.matches();
+        }).collect(Collectors.toList());
 
         Map<Integer, Vertex<Integer>> vertexMap = new HashMap<>(vertexStringLines.size());
 
+        // Add the vertices to the graph.
         for (int i = 1; i <= vertexStringLines.size(); i++) {
             Vertex<Integer> v = new Vertex<>(i);
             vertexMap.put(i, v);
@@ -45,13 +54,14 @@ public class GraphLoader {
 
         lines = Files.lines(path);
 
-        List<String> edgeStringLines = lines
-                                               .filter((line) -> {
-                                                   Pattern p = Pattern.compile("\\d+ \\d+");
-                                                   Matcher m = p.matcher(line);
-                                                   return m.matches();
-                                               })
-                                               .collect(Collectors.toList());
+        // Get all the edges of the graph.
+        List<String> edgeStringLines = lines.filter((line) -> {
+            Pattern p = Pattern.compile("\\d+ \\d+");
+            Matcher m = p.matcher(line);
+            return m.matches();
+        }).collect(Collectors.toList());
+
+        // Add all the edges to the graph.
         for (String line : edgeStringLines) {
             StringTokenizer st = new StringTokenizer(line);
             String v1 = st.nextToken();
@@ -59,44 +69,52 @@ public class GraphLoader {
             graph.addEdge(new Edge<>(v1 + "-" + v2), vertexMap.get(Integer.valueOf(v1)), vertexMap.get(Integer.valueOf(v2)));
         }
 
+        // Set the degree of all the vertex objects.
         graph.getVertices().forEach(v -> v.setDegree(graph.degree(v)));
 
         return graph;
     }
 
+    /**
+     * Creates a JUNG graph from a graph in a file that specifies only the edges of a graph.
+     *
+     * @param fileLocation The location of the graph file.
+     * @return A graph object that represents the graph specified in the file.
+     * @throws IOException
+     */
     public static Graph<Vertex<Integer>, Edge<String>> createGraphFromFileEdgesOnly(String fileLocation) throws IOException {
         Graph<Vertex<Integer>, Edge<String>> graph = new SparseGraph<>();
         Path path = Paths.get(fileLocation);
 
         Stream<String> lines = Files.lines(path);
+        HashMap<Integer, Vertex<Integer>> vertexMap = new HashMap<>();
 
-        Vertex[] vertexArray = new Vertex[14855842];
-
-        Random random = new Random();
-
+        // For each line in the file, add the vertices if they are not already included,
+        // and create the edge in the graph.
         lines.forEach(line -> {
             StringTokenizer st = new StringTokenizer(line);
             Integer v1 = Integer.valueOf(st.nextToken());
             Integer v2 = Integer.valueOf(st.nextToken());
 
-            Vertex<Integer> vertex1 = vertexArray[v1];
-            Vertex<Integer> vertex2 = vertexArray[v2];
+            Vertex<Integer> vertex1 = vertexMap.get(v1);
+            Vertex<Integer> vertex2 = vertexMap.get(v2);
 
             if (vertex1 == null) {
                 vertex1 = new Vertex<>(v1);
-                vertexArray[v1] = vertex1;
+                vertexMap.put(v1, vertex1);
                 graph.addVertex(vertex1);
             }
 
             if (vertex2 == null) {
                 vertex2 = new Vertex<>(v2);
-                vertexArray[v2] = vertex2;
+                vertexMap.put(v2, vertex2);
                 graph.addVertex(vertex2);
             }
 
             graph.addEdge(new Edge<>(v1 + "-" + v2), vertex1, vertex2);
         });
 
+        // Set the degree of all the vertex objects.
         graph.getVertices().forEach(v -> v.setDegree(graph.degree(v)));
 
         return graph;
