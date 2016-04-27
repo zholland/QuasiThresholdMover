4d7f9b03099996b932b7f6f0a1b86b0b51f5ea8b
zach <zacharias.holland@gmail.com>
2016-04-27 12:52:25 -0700

    Added some comments to the QtmTester file.

--- qtm/src/drivers/QtmTester.java
+++ qtm/src/drivers/QtmTester.java
@@ -10,9 +10,19 @@ import utility.GraphEditCounter;
 import java.io.IOException;
 import java.util.ArrayList;
 
+/**
+ * Driver used to run trials of the qtm algorithm on various graphs.
+ * Provides some statistics about the results.
+ *
+ * @author Zach Holland
+ */
 public class QtmTester {
+    // The number of trials to run.
     public static final int ITERATIONS = 100;
 
+    /**
+     * A data transfer object that tracks the number of edits and the time.
+     */
     private static class TimeEditsDto {
         private long _time;
         private int _edits;
@@ -33,15 +43,16 @@ public class QtmTester {
 
     public static void main(String[] args) {
         try {
-            //Graph<Vertex<Integer>, Edge<String>> graph = GraphLoader.createGraphFromFile(args[0]);
-
             ArrayList<Integer> numEditsList = new ArrayList<>();
 
             long timeSum = 0L;
             int editsSum = 0;
             int minEdits = Integer.MAX_VALUE;
+
+            // Do a number of trials equal to ITERATIONS
             for (int i = 0; i < ITERATIONS; i++) {
                 TimeEditsDto timeEditsDto = doMover(GraphLoader.createGraphFromFile("/home/zach/IdeaProjects/QuasiThresholdMover/qtm/graphs/karate.paj"), timeSum);
+                // Get the time and edits from the trial and track the minimum.
                 timeSum += timeEditsDto.getTime();
                 editsSum += timeEditsDto.getEdits();
                 numEditsList.add(timeEditsDto.getEdits());
@@ -57,13 +68,15 @@ public class QtmTester {
             for (Integer edits : numEditsList) {
                 diffSum += Math.pow(edits - avgEdits, 2);
             }
-
             double std = Math.sqrt(diffSum / (double) ITERATIONS);
 
+            // Display the statistics.
             System.out.println("Min edits: " + minEdits);
             System.out.println("Avg edits: " + avgEdits);
             System.out.println("STD: " + std);
             System.out.println("Avg time(ms): " + (double) timeSum / (double) ITERATIONS);
+
+            // Uncomment to visualize with JUNG
             // GraphViewer.showGraph(resultGraph);
         } catch (IOException e) {
             System.out.println("Unable to load graph!");
@@ -71,8 +84,14 @@ public class QtmTester {
         }
     }
 
+    /**
+     * Runs the qtm algorithm on the given graph. Returns the time it took and the number of edits.
+     *
+     * @param graph   The graph to run the algorithm on.
+     * @param timeSum The total sum of the time.
+     * @return A dto with the time it took for the trial and the number of edits.
+     */
     private static TimeEditsDto doMover(Graph<Vertex<Integer>, Edge<String>> graph, Long timeSum) {
-//        System.out.println("Loaded");
         long startTime = System.nanoTime();
         QuasiThresholdMover<Integer> qtm = new QuasiThresholdMover<>(graph, Integer.MAX_VALUE);
         Graph<Integer, String> resultGraph = qtm.doQuasiThresholdMover(true, false);
@@ -81,8 +100,11 @@ public class QtmTester {
         int edits = GraphEditCounter.numberOfEditsAfterFinished(graph, resultGraph);
 
         long time = (endTime - startTime) / 1000000;
+
+        // Uncomment to show the time and edits for each trial.
         //System.out.println("Edits: " + edits);
         //System.out.println("Time(ms): " + time);
+
         return new TimeEditsDto(time, edits);
     }
 }
